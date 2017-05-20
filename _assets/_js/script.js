1fefdad59f813ebe8643a62e1a65df34fb4372dd
solsnhan <solangenhan@Solanges-MBP.T-mobile.com>
2017-05-19 18:49:48 -0700

    nav update

--- _assets/_js/script.js
+++ _assets/_js/script.js
@@ -1,7 +1,31 @@
-$('#v').click(
-    function (e) {
-   	 $('html, body').animate({scrollTop: $('#about').position().top}, 600);
-   	 return false;
-    }
- );
+$(document).ready(function() {
+
+    //-----------NAVIGATION------------//
+      var $toggleButton = $('.toggle-button'),
+          $menuWrap = $('.mainnav'),
+          $sidebarArrow = $('.sidebar-menu-arrow');
+
+      var navLink = $sidebarArrow.parent();
+
+      // Dropdown button
+      $(navLink).click(function() {
+          
+          $(this).children().next().next().slideToggle(300);
+      });
+
+      // Hamburger button
+
+      $toggleButton.on('click', function() {
+        $(this).toggleClass('button-open');
+        $menuWrap.toggleClass('menu-show');
+      });
+
+	$('#v').click(
+    	function (e) {
+   		 $('html, body').animate({scrollTop: $('#about').position().top}, 600);
+   		 return false;
+   	 }
+   	 );
+
+});
 
